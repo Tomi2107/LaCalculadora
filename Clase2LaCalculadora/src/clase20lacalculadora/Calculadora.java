78540f1d7f9350dd3cdc325c96aea42b5047b6ea
Tomi2107 <83030653+Tomi2107@users.noreply.github.com>
2022-02-20 20:09:06 -0300

    Add files via upload

---
+++ Clase2LaCalculadora/src/clase20lacalculadora/Calculadora.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase20lacalculadora;
+
+/**
+ *
+ * @author tomas
+ */
+public class Calculadora {
+    public double Suma(double a,double b){
+        return a+b;
+    }
+    public double Resta(double a,double b){
+        return a-b;
+    }
+    public double Multiplicacion(double a,double b){
+        return a*b;
+    }
+    public double Division(double a,double b){
+        return a/b;
+    }
+}
